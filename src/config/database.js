79d978b4862dcef3346b32450f71929fa9066981
Angelo Cerqueira <angelosc1998@gmil.com>
2019-11-10 14:09:03 -0400

    iniciando back-end

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: '192.168.99.100',
+  username: 'postgres',
+  password: '20003807',
+  database: 'goBarber',
+  define: {
+    timestamp: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
