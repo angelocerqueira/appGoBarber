34a2e68bd887d29e760a3c3883d8e987e35189af
Angelo Cerqueira <angelosc1998@gmil.com>
2019-11-26 15:32:46 -0400

    feat: Listagem dos provedores de serviços

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import multer from 'multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 import authMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
@@ -17,6 +18,7 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 routes.put('/users');
+routes.get('/providers', ProviderController.index);
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
