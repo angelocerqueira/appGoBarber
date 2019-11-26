a25aa23607777ed532eb562ef379faf8eb624e4c
Angelo Cerqueira <angelosc1998@gmil.com>
2019-11-26 18:57:55 -0400

    Feat: Listando agenda do prestador

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
@@ -18,10 +19,13 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
-routes.put('/users');
+
 routes.get('/providers', ProviderController.index);
-routes.post('/files', upload.single('file'), FileController.store);
+
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+
+routes.get('/schedule', ScheduleController.index);
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
