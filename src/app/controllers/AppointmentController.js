9021088c18128d35dfbf213693e90b0aa4c6a047
Angelo Cerqueira <angelosc1998@gmil.com>
2019-11-27 18:00:37 -0400

    exception  Handling

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -11,12 +11,12 @@ import CancellationMail from '../jobs/cancellationMail';
 class AppointmentController {
   async index(req, res) {
     const { page = 1 } = req.query;
-    const appointments = await Appointment.findAll({
+    const appointments = await Appointment.finddAll({
       where: { user_id: req.userId, canceled_at: null },
       order: ['date'],
       limit: 20,
       offset: (page - 1) * 20,
-      attributes: ['id', 'date'],
+      attributes: ['id', 'date', 'past', 'cancelable'],
       include: [
         {
           model: User,
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://d8f6b9c7b26b4e41bf02b859c6d30c18@sentry.io/1836853',
+};
